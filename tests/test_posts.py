0781d15382a7855c39ccfdd5cc579ddc422c065d
RainersGITHub <Rainer.Rode@web.de>
2024-04-29 10:19:34 +0300

    modified 20240429

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,7 @@
 from typing import List
+
+import pytest
+
 from app import schemas
 
 
@@ -21,6 +24,38 @@ def test_unauthorized_user_get_all_posts(client, test_posts):
     assert result.status_code == 401
 
 
-def test_unauthorized_user_get_one_post(client, test_posts):
+def test_unauthorized_user_get_post_by_id(client, test_posts):
     result = client.get(f"/posts/{test_posts[0].id}")
     assert result.status_code == 401
+
+
+def get_post_by_not_existing_id(authorized_client, test_posts):
+    result = authorized_client.get("/posts/88888")
+    assert result.status_code == 404
+
+
+def get_post_by_existing_id(authorized_client, test_posts):
+    result = authorized_client.get(f"/posts/{test_posts[0].id}")
+
+    post = schemas.PostJoin(**result.json())
+
+    assert result.status_code == 200
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
+
+
+@pytest.mark.parametrize("title, content, published",
+                         [("awesome new title", "awesome new content", True),
+                          ("favourite pizza", "I love peperoni", False),
+                          ("tallest skyscraper", "wahoo", True),
+                          ])
+def test_create_post(authorized_client, create_test_user, test_posts, title, content, published):
+    result = authorized_client.post("/posts/", json={"title": title, "content": content, "published": published})
+
+    created_post = schemas.PostCreate(**result.json())
+
+    assert result.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
